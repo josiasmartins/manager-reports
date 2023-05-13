4ebf59fcf078be236ea9dc88543cfd6393e8aa5e
Caitano <josiasmartinscaitano@gmail.com>
2023-05-13 12:43:10 -0300

    feat: install express and creat method to send file index.html

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3001;
+
+// run static content the folder "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+
+// execute file index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+})
+
+// run the server
+app.listen(port, () => {
+  console.log(`server run in port ${port}`);
+})
