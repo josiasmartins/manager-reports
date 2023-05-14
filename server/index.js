9c8df9cc1775812b56ffd00c5c142bbe9f5fbd57
Caitano <josiasmartinscaitano@gmail.com>
2023-05-13 21:44:04 -0300

    feat: creat template and add style

--- server/index.js
+++ server/index.js
@@ -5,8 +5,11 @@ const app = express();
 const port = 3001;
 
 // run static content the folder "public"
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
+
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // execute file index.html
