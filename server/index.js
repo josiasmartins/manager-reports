ea44d316c39dd9072a1efc8705e365a828c2322a
Caitano <josiasmartinscaitano@gmail.com>
2023-05-16 11:35:33 -0300

    refactor: back again

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const router = express.Router;
+// const router = express.Router;
 
 const app = express();
 const port = process.env.PORT || 3001;
